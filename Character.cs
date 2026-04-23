c26f1df17ebacfb03f04585b3be6fe45b3aab483
victormellos <victormellos@outlook.com>
2026-04-23 16:41:33 -0300

    Creating Character class

--- Character.cs
+++ Character.cs
@@ -1,6 +1,12 @@
 namespace battle;
 
-class Character
+public class Character : Entity
 {
+    public string Name { get; set; }
+
+    public Character(string name) : base(maxHealth: 100) 
+    {
+        Name = name;
     
+    }
 }
